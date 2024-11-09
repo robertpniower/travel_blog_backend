cef633f9b8d80611737a3c53ed6c70e868b69fc7
robertpniower <robert.pniower@deliveryhero.com>
2024-11-09 16:28:06 +0100

    add eslint rules

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,12 +1,12 @@
 const connection = require('../config/db.js');
 
-
 class CountryController {
 
     static async getCountries(req, res) {
         try {
             const query = `SELECT * FROM countries`;
             const [result] = await connection.query(query);
+
             res.status(200).json(result);
         } catch (error) {
             console.error(error);
@@ -20,12 +20,13 @@ class CountryController {
             const query = "INSERT INTO countries (country_code, country) VALUES(?, ?)";
             const values = [country_code, country];
             const [result] = await connection.execute(query, values);
+
             return result.insertId;
         } catch (err) {
             console.error('Error inserting Country: ', err)
         }
     }
-    
+
 }
 
 module.exports = CountryController;
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const ArticleControlller = require('../controllers/articelController')
-
 
 router.get('/articles', ArticleControlller.getAllArticles)
 router.get('/articles/icon/:articleId', ArticleControlller.getArticleIcon);
